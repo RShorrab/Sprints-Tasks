f0082b9ecc7783a0358a8bf5d45844fc3eb922de
Rawan <rshorrab1@gmail.com>
2023-05-18 18:38:14 +0200

    bug fixed

--- Sprint 3/Task 3/index.js
+++ Sprint 3/Task 3/index.js
@@ -1,16 +1,18 @@
 var list = document.getElementById("list");
 var button = document.getElementById("show-button")
 
+list.style.display = "none" //element.style != none!!!
+
 function btntog() 
 {
-    if (list.style.display === "none") 
-    {
-      list.style.display = "block";
-      button.innerHTML = "Less!"
-    } 
-    else 
-    {
-      list.style.display = "none";
-      button.innerHTML = "Show me!"
-    }
+  if (list.style.display === "none") 
+  {
+    list.style.display = "block";
+    button.innerHTML = "Less!"
+  } 
+  else 
+  {
+    list.style.display = "none";
+    button.innerHTML = "Show me!"
+  }
 }
