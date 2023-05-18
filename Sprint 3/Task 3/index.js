1c39cec2a584e083d92321637191ee8f2bf233dd
Rawan <rshorrab1@gmail.com>
2023-05-18 18:25:11 +0200

    update

--- Sprint 3/Task 3/index.js
+++ Sprint 3/Task 3/index.js
@@ -1,7 +1,8 @@
+var list = document.getElementById("list");
+var button = document.getElementById("show-button")
+
 function btntog() 
 {
-    var list = document.getElementById("list");
-    var button = document.getElementById("show-button")
     if (list.style.display === "none") 
     {
       list.style.display = "block";
