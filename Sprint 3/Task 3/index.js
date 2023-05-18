1ff1c65b254ab9aca421219e64b96a90c4fc5f04
Rawan <rshorrab1@gmail.com>
2023-05-18 18:09:22 +0200

    Add Task 3 (Top Songs)

---
+++ Sprint 3/Task 3/index.js
@@ -0,0 +1,9 @@
+function btntog() 
+{
+    var x = document.getElementById("list");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
