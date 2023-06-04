d39f5a928020c43c0f5c67ff301578b5c5718929
Rawan <rshorrab1@gmail.com>
2023-06-04 18:39:30 +0200

    Update icons

--- Sprint 5/Task 4, 5/js/main.js
+++ Sprint 5/Task 4, 5/js/main.js
@@ -190,14 +190,14 @@ let sortingByPriority = document.getElementById("sortingByPriority")
         sortAscByPriority(tasks)
         sortingByPriority.classList.remove("Asc")
         sortingByPriority.classList.add("Des")
-        sortingByName.innerHTML = `Task Name <span> <i class="fa-solid fa-caret-up"></i> </span>`
+        sortingByName.innerHTML = `Task Priority <span> <i class="fa fa-caret-down" aria-hidden="true"></i> </span>`
     }
     else if(sortingByPriority.classList.contains("Des"))
     {
         sortDesByPriority(tasks)
         sortingByPriority.classList.remove("Des")
         sortingByPriority.classList.add("Asc")
-        sortingByName.innerHTML = `Task Name <span> <i class="fa-solid fa-caret-down"></i> </span>`
+        sortingByName.innerHTML = `Task Priority <span> <i class="fa fa-caret-up" aria-hidden="true"></i> </span>`
     }
 })
 
@@ -208,14 +208,14 @@ let sortingByName = document.getElementById("sortingByName")
         sortAscByPriority(tasks)
         sortingByName.classList.remove("Asc")
         sortingByName.classList.add("Des")
-        sortingByName.innerHTML = `Task Name <span> <i class="fa-solid fa-caret-up"></i> </span>`
+        sortingByName.innerHTML = `Task Name <span> <i class="fa fa-caret-down" aria-hidden="true"></i> </span>`
     }
     else if(sortingByName.classList.contains("Des"))
     {
         sortDesByPriority(tasks)
         sortingByName.classList.remove("Des")
         sortingByName.classList.add("Asc")
-        sortingByName.innerHTML = `Task Name <span> <i class="fa-solid fa-caret-down"></i> </span>`
+        sortingByName.innerHTML = `Task Name <span> <i class="fa fa-caret-up" aria-hidden="true"></i> </span>`
     }
 })
 
